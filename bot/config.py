13cef7c04bf95d8b388a1c3911497d6c034e57fc
orlandoc0107 <orlandocardenas0107@gmail.com>
2024-12-11 13:43:04 -0300

    update:github

--- bot/config.py
+++ bot/config.py
@@ -5,7 +5,6 @@ load_dotenv()
 
 class Configuration(metaclass=MetaFlaskEnv):
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-    NGROK_TOKEN = os.getenv("NGROK_TOKEN")
     NGROK_URL = os.getenv("NGROK_URL")
     PORT = int(os.getenv("PORT", 5000))
     DEBUG = os.getenv("DEBUG", "false").lower() == "true"
@@ -13,7 +12,6 @@ class Configuration(metaclass=MetaFlaskEnv):
 
     # Imprime las variables de entorno para verificar si están cargadas correctamente
     print("TELEGRAM_TOKEN:", TELEGRAM_TOKEN)
-    print("NGROK_TOKEN:", NGROK_TOKEN)
     print("NGROK_URL:", NGROK_URL)
     print("PORT:", PORT)
     print("DEBUG:", DEBUG)
